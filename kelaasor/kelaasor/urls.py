57f2cf2357867bd264f5d8588f7121d30400ab07
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-17 20:21:28 +0330

    🎉 main urls added

--- kelaasor/kelaasor/urls.py
+++ kelaasor/kelaasor/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('bootcamp/', include('bootcamp.urls')),
+    path('support/', include('support.urls')),
 ]
