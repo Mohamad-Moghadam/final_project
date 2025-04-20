66ead172d4f84e2aab83e26444ac188c4b899379
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-20 18:23:20 +0330

    🚨 url fixed

--- kelaasor/user/urls.py
+++ kelaasor/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import NewHeadmaster, ListHeadmasters
+from user.views import NewHeadmaster, ListHeadmasters, DeleteHeadmaster
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('sign-up', TokenObtainPairView.as_view()),
     path('new-headmaster', NewHeadmaster.as_view()),
     path('ls-headmasters', ListHeadmasters.as_view()),
+    path('del-headmaster/<int:pk>', DeleteHeadmaster.as_view()),
 ]
