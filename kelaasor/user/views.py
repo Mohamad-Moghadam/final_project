7317cdbf895f29d1ace2cab39429091d3cc9ca51
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-20 05:10:02 +0330

    🔧 superuser permissions and adding a superuser added

--- kelaasor/user/views.py
+++ kelaasor/user/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import BasePermission, AllowAny
 from serializers import UserSerializer
 
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+class NewHeadmaster(CreateAPIView):
+    permission_classes= [IsSuperUser]
+    queryset= User
+    serializer_class= UserSerializer
 
 class SignUp(CreateAPIView):
     permission_classes= [AllowAny]
     queryset= User
     serializer_class= UserSerializer
+
