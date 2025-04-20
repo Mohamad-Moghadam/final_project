eae8e200fa3e38cc4473112ce0cde6bf82a479b3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-20 16:52:53 +0330

    🔧 def perform created added

--- kelaasor/user/views.py
+++ kelaasor/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import BasePermission, AllowAny
-from serializers import UserSerializer
+from user.serializers import UserSerializer
 
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
@@ -12,6 +12,13 @@ class NewHeadmaster(CreateAPIView):
     permission_classes= [IsSuperUser]
     queryset= User
     serializer_class= UserSerializer
+    def perform_create(self, serializer):
+        user= serializer.save()
+        user.is_superuser= True
+        user.is_staff= True
+        user.save()
+    def __str__(self):
+        print(self.user)
 
 class SignUp(CreateAPIView):
     permission_classes= [AllowAny]
