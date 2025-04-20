7990884a8069648090b05aafc438e909d37cbcd5
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-20 17:01:22 +0330

    ✨ viewing headmasters url added

--- kelaasor/user/urls.py
+++ kelaasor/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import NewHeadmaster
+from user.views import NewHeadmaster, ListHeadmasters
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('sign-up', TokenObtainPairView.as_view()),
-    path('new-headmaster', NewHeadmaster.as_view())
+    path('new-headmaster', NewHeadmaster.as_view()),
+    path('ls-headmasters', ListHeadmasters.as_view()),
 ]
