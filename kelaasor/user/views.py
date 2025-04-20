8167e4bf6b1067bc92ac7612d560d0d1a4c6e43b
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-20 17:00:08 +0330

    ✨ viewing headmasters view added

--- kelaasor/user/views.py
+++ kelaasor/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import BasePermission, AllowAny
 from user.serializers import UserSerializer
 
@@ -25,3 +25,10 @@ class SignUp(CreateAPIView):
     queryset= User
     serializer_class= UserSerializer
 
+class ListHeadmasters(RetrieveAPIView):
+    permission_classes= [IsSuperUser]
+    queryset= User.objects.filter(is_superuser= True)
+    serializer_class= UserSerializer
+
+class NewTech(CreateAPIView):
+    pass
