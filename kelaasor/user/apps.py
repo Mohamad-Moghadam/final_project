65bd658a45ae95b482773277583583422790ac38
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-22 20:53:15 +0330

    🐛 ready method fixed

--- kelaasor/user/apps.py
+++ kelaasor/user/apps.py
@@ -5,8 +5,16 @@ class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
 
-"""class AddingHeadmaster(AppConfig):
-    name= "user"
 
     def ready(self):
-        import user.signals"""
+        import user.signals
+        from django.db.utils import OperationalError
+        from django.contrib.auth.models import Group
+
+
+        try:
+            Group.objects.get_or_create(name='HeadMaster')
+            Group.objects.get_or_create(name='Technicians')
+
+        except OperationalError as e:
+            print(f"Error: {e}")
