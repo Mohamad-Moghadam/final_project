e82fb44e1c0108f4c258f8e7ef3dc33feb93da6d
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-22 17:26:39 +0330

    🐛 views' bugs fixed

--- kelaasor/user/views.py
+++ kelaasor/user/views.py
@@ -1,76 +1,66 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, DestroyAPIView
 from rest_framework.permissions import BasePermission, AllowAny, IsAuthenticated
 from user.serializers import UserSerializer
-from user.models import HeadMaster
+from user.models import HeadMaster, Technicians
 from rest_framework.views import APIView
+from user.permissions import IsHeadmaster, IsTechnician
 
-class IsSuperUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_superuser
-
-class IsTech(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in ['PATCH', 'PUT', 'GET', 'DELETE', 'POST']:
-            if request.user.is_staff or request.user.is_superuser:
-                return False
-        else:
-            return True
 
 class NewHeadmaster(CreateAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User
+    permission_classes= [IsHeadmaster]
+    queryset= HeadMaster.objects.all()
     serializer_class= UserSerializer
     def perform_create(self, serializer):
         user= serializer.save()
         user.is_superuser= True
         user.is_staff= True
+        headmaster_group= Group.objects.get(name= 'HeadMaster')
+        user.groups.add(headmaster_group)
         user.save()
-    def __str__(self):
-        print(self.user)
 
 class SignUp(CreateAPIView):
     permission_classes= [AllowAny]
-    queryset= User
+    queryset= User.objects.all()
     serializer_class= UserSerializer
 
 class ListHeadmasters(ListAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.filter(is_superuser= True)
+    permission_classes= [IsHeadmaster]
+    queryset= HeadMaster.objects.filter(is_superuser= True)
     serializer_class= UserSerializer
 
 
 class DeleteHeadmaster(DestroyAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.filter(is_superuser= True)
+    permission_classes= [IsHeadmaster]
+    queryset= HeadMaster.objects.filter(is_superuser= True)
     serializer_class= UserSerializer
 
 class RetrieveHeadmaster(RetrieveAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.filter(is_superuser= True)
+    permission_classes= [IsHeadmaster]
+    queryset= HeadMaster.objects.filter(is_superuser= True)
     serializer_class= UserSerializer
 
 class NewTech(CreateAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.all()
+    permission_classes= [IsHeadmaster]
+    queryset= Technicians.objects.all()
     serializer_class= UserSerializer
 
     def perform_create(self, serializer):
         user= serializer.save()
         user.is_staff= True
+        technician_group= Group.objects.get(name= 'Technicians')
+        user.groups.add(technician_group)
         user.save()
-    def __str__(self):
-        return f"{self.user}"
 
 class DelTech(DestroyAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.filter(is_staff= True)
+    permission_classes= [IsHeadmaster]
+    queryset= Technicians.objects.filter(is_staff= True)
     serializer_class= UserSerializer
 
 class LsTech(ListAPIView):
-    permission_classes= [IsSuperUser]
-    queryset= User.objects.filter(is_staff= True, is_superuser= False)
+    permission_classes= [IsHeadmaster]
+    queryset= Technicians.objects.filter(is_staff= True, is_superuser= False)
     serializer_class= UserSerializer
 
 class NewUser(CreateAPIView):
@@ -79,7 +69,7 @@ class NewUser(CreateAPIView):
     serializer_class= UserSerializer
 
 class LogIn(APIView):
-    permission_classes= [IsAuthenticated]
+    permission_classes= [IsAuthenticated, IsTechnician]
 
     def post(self, request):
         return render(request, 'user_panel/user_panel.html', {'user': request.user})
