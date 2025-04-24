37aac6573887102430ed3e966a1652ad4a2b3162
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-24 19:11:36 +0330

    ✨ bootcamp ls view added

--- kelaasor/bootcamp/views.py
+++ kelaasor/bootcamp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from bootcamp.models import BootCamps
+from bootcamp.serializers import BootcampSerializer
 
-# Create your views here.
+
+class LsBootcamps(ListAPIView):
+    permission_classes= [AllowAny]
+    queryset= BootCamps.objects.all()
+    serializer_class= BootcampSerializer
