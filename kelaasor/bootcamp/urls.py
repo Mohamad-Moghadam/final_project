d047f5cdb63ab521bb17ec470515035f3dc31207
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-25 15:15:55 +0330

    ✨ endpoint for newbootcamp added

--- kelaasor/bootcamp/urls.py
+++ kelaasor/bootcamp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from bootcamp.views import LsBootcamps
+from bootcamp.views import LsBootcamps,NewBootcamp
 
 urlpatterns = [
     path('ls-bootcamps', LsBootcamps.as_view()),
+    path('new-bootcamp', NewBootcamp.as_view()),
 ]
--- kelaasor/bootcamp/views.py
+++ kelaasor/bootcamp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import AllowAny
 from bootcamp.models import BootCamps
 from bootcamp.serializers import BootcampSerializer
+from user.permissions import IsHeadmaster
 
 
 class LsBootcamps(ListAPIView):
     permission_classes= [AllowAny]
+    queryset= BootCamps.objects.all()
+    serializer_class= BootcampSerializer
+
+class NewBootcamp(CreateAPIView):
+    permission_classes= [IsHeadmaster]
     queryset= BootCamps.objects.all()
     serializer_class= BootcampSerializer
