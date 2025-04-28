0096a9a0435eaae6c6f5b9b65e8b25fe960c3c9b
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-28 20:24:29 +0330

    ✨ serializer model added

---
+++ kelaasor/support/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from support.models import Ticket
+
+
+class TicketSerializer(ModelSerializer):
+    class Meta:
+        model= Ticket
+        fields= '__all__'
