e85877e549ee6ecc06612798cc027600970b4988
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-28 20:39:49 +0330

    ✨ response ticket serializer added

--- kelaasor/support/serializers.py
+++ kelaasor/support/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from support.models import Ticket
+from support.models import Ticket, ResponseTicket
 
 
 class TicketSerializer(ModelSerializer):
     class Meta:
         model= Ticket
+        fields= '__all__'
+        read_only_fields= ['user']
+
+class ResponseTicketSerializer(ModelSerializer):
+    class Meta:
+        model= ResponseTicket
         fields= '__all__'
