91256f9a0ffca7fa64ecb674ebda7d4cea232619
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-28 20:41:14 +0330

    ✨ adding a response view

--- kelaasor/support/views.py
+++ kelaasor/support/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from support.models import Ticket
-from support.serializers import TicketSerializer
+from support.models import Ticket, ResponseTicket
+from support.serializers import TicketSerializer, ResponseTicketSerializer
+from user.permissions import IsTechnician
 
 class SendTicket(CreateAPIView):
     permission_classes= [IsAuthenticated]
@@ -11,3 +12,8 @@ class SendTicket(CreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(user= self.request.user)
+
+class Response(CreateAPIView):
+    permission_classes= [IsTechnician]
+    queryset= ResponseTicket.objects.all()
+    serializer_class= ResponseTicketSerializer
