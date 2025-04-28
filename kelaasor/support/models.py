be0788e2c566d0d01a9f43695bc34c31812092c8
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-28 20:39:08 +0330

    🚨 user field added

--- kelaasor/support/models.py
+++ kelaasor/support/models.py
@@ -4,6 +4,7 @@ class Ticket(models.Model):
     TITLE= ['Sign-up issue', 'Log-in issue', 'Purchase issue', 'Other']
     title= models.Choices(*TITLE)
     content= models.TextField()
+    user= models.CharField(max_length= 100)
 
 class ResponseTicket(models.Model):
     question= models.ForeignKey(Ticket, on_delete= models.CASCADE, related_name= 'Response_of_each_ticket')
