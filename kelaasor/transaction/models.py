4397ffa7d030dc7a60af5e34284323f07eca9b43
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-02 19:55:57 +0330

    ✨ models added

--- kelaasor/transaction/models.py
+++ kelaasor/transaction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    balance = models.PositiveBigIntegerField(default=0)
+
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    to = models.ForeignKey(Wallet, on_delete= models.CASCADE)
