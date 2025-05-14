a16d3266c5c96ceb91e7098b5650769baeafb500
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 18:33:16 +0330

    🐛 models fixed

--- kelaasor/blog/models.py
+++ kelaasor/blog/models.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
+    STATUS= [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
     title = models.CharField(max_length=255)
     content = models.TextField()
+    type = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name= 'author')
     created_at = models.DateTimeField(auto_now_add=True)
+    status= models.CharField(max_length=10, choices= STATUS, default= 'draft')
