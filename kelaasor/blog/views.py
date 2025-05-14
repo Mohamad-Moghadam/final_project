cccc3482879e14fc3209983a55c89b60c416b4d0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 18:34:14 +0330

    ✨ viewing all blogs and published blogs added

--- kelaasor/blog/views.py
+++ kelaasor/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
-from user.permissions import IsTechnician
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView
+from user.permissions import IsTechnician, IsHeadmaster
 from .models import Blog
 from .serializers import BlogSerializer
+from rest_framework.permissions import AllowAny
 
 
 class NewBlog(CreateAPIView):
@@ -12,3 +13,15 @@ class NewBlog(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author= self.request.user)
+
+
+class PublishedBlogs(ListAPIView):
+    permission_classes= [AllowAny]
+    queryset= Blog.objects.filter(status= 'published')
+    serializer_class= BlogSerializer
+
+
+class ControllBlogs(ListAPIView):
+    permission_classes= [IsHeadmaster]
+    queryset= Blog.objects.all()
+    serializer_class= BlogSerializer
