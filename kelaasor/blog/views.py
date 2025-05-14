662b06e0c134fb9daea5550b495247e8f0e63dca
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 18:41:30 +0330

    ✨ editting blog view added

--- kelaasor/blog/views.py
+++ kelaasor/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView
 from user.permissions import IsTechnician, IsHeadmaster
 from .models import Blog
 from .serializers import BlogSerializer
@@ -25,3 +25,11 @@ class ControllBlogs(ListAPIView):
     permission_classes= [IsHeadmaster]
     queryset= Blog.objects.all()
     serializer_class= BlogSerializer
+
+
+class EditBlog(UpdateAPIView):
+    permission_classes= [IsTechnician]
+    serializer_class= BlogSerializer
+
+    def get_queryset(self):
+        return Blog.objects.filter(author= self.request.user)
