1944b40371d25b383ff54f141f0ca03bc174fcf4
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 18:42:13 +0330

    ✨ editting blog url added

--- kelaasor/blog/urls.py
+++ kelaasor/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import NewBlog, PublishedBlogs, ControllBlogs
+from .views import NewBlog, PublishedBlogs, ControllBlogs, EditBlog
 
 
 urlpatterns = [
     path('add-blog', NewBlog.as_view()),
     path('show-blogs', PublishedBlogs.as_view()),
     path('controll-blogs', ControllBlogs.as_view()),
+    path('edit-blog/<int:pk>', EditBlog.as_view()),
 ]
