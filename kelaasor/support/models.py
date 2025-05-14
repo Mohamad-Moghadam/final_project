8b87aacad1fe73eff1b0367184aea9475146eecc
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 19:29:14 +0330

    🐛 status of the tickets fixed

--- kelaasor/support/models.py
+++ kelaasor/support/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Ticket(models.Model):
-    TITLE= ['Sign-up issue', 'Log-in issue', 'Purchase issue', 'Other']
-    title= models.Choices(*TITLE)
+    STATUS= [('pending', 'Pending'), ('resolved', 'Resolved'), ('rejected', 'Rejected'), ('closed', 'Closed')]
+    TITLE= [('technical', 'Technical'), ('financial', 'Financial'), ('general', 'General')]
+    title= models.CharField(max_length= 100, choices= TITLE)
     content= models.TextField()
     user= models.CharField(max_length= 100)
+    status= models.CharField(max_length= 100, choices= STATUS, default= 'pending')
 
 class ResponseTicket(models.Model):
     question= models.ForeignKey(Ticket, on_delete= models.CASCADE, related_name= 'Response_of_each_ticket')
