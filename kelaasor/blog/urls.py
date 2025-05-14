d033d5ebefd5fdc18090cdd77f3f633329d1e5aa
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-05-14 18:34:44 +0330

    ✨ all urls added

--- kelaasor/blog/urls.py
+++ kelaasor/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import NewBlog
+from .views import NewBlog, PublishedBlogs, ControllBlogs
 
 
 urlpatterns = [
     path('add-blog', NewBlog.as_view()),
+    path('show-blogs', PublishedBlogs.as_view()),
+    path('controll-blogs', ControllBlogs.as_view()),
 ]
