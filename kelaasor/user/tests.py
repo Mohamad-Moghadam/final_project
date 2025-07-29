5b2f36f94d121b2f7c9c0c935ab0878d4d40f48c
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-07-29 17:20:13 +0330

    feat: add test for user app createusertest

--- kelaasor/user/tests.py
+++ kelaasor/user/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User, Group
+from rest_framework import status
+from django.urls import reverse
+
+
+
+
+
+class CreateUserTest(APITestCase):
+    def test_create_user(self):
+        url = reverse('create-user')
+        data = {
+            "username":"testali21",
+            "email":"testali21@email.com",
+            "password":"testali1122"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["username"], 'testali21')
+
+
+
